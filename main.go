f599c65e8108a07283617b19de9c618ae0867f40
Gianluca <gianluca.dimundo@gmail.com>
2025-07-25 21:51:16 -0400

    feat: add tilemap structure - part 1

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"image"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+const (
+	TileSize int = 16
 )
 
 type Game struct{}
@@ -15,14 +20,51 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, "Hello, World!")
+	scene := GetSceneTileMapFile()
+
+	tilemap, err := LoadScene(scene[Shire].tilemapJson)
+
+	tileset, _, err := ebitenutil.NewImageFromFile("assets/tilesets/" + scene[Shire].tilesetName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	opts := ebiten.DrawImageOptions{}
+
+	for _, layer := range tilemap.Layers {
+		for index, tileId := range layer.Data {
+			x := index % layer.Width
+			y := index / layer.Width
+
+			x *= TileSize
+			y *= TileSize
+
+			srcX := (tileId - 1) % 32
+			srcY := (tileId - 1) / 32
+
+			srcX *= TileSize
+			srcY *= TileSize
+
+			opts.GeoM.Translate(float64(x), float64(y))
+
+			screen.DrawImage(
+				tileset.SubImage(image.Rect(srcX, srcY, srcX+TileSize, srcY+TileSize)).(*ebiten.Image),
+				&opts,
+			)
+
+			opts.GeoM.Reset()
+		}
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	return 640, 480
 }
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 	ebiten.SetWindowTitle("Hello, World!")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
--- scene.go
+++ scene.go
@@ -1,15 +1,46 @@
-package main 
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type SceneId int
+
+const (
+	Shire SceneId = 1
+)
 
 type Scene struct {
-  Name string
+	tilesetName string
+	tilemapJson string
+}
+
+func GetSceneTileMapFile() map[SceneId]Scene {
+	return map[SceneId]Scene{
+		Shire: Scene{tilesetName: "sample.png", tilemapJson: "shire.json"},
+	}
 }
 
 type TilemapLayer struct {
-  Data []int `json:"data"`
-  Width int `json:"width"`
-  Height int `json:"height"`
-} 
+	Data   []int `json:"data"`
+	Width  int   `json:"width"`
+	Height int   `json:"height"`
+}
 
 type Tilemap struct {
-  Layers []TilemapLayer `json:"layers"`
+	Layers []TilemapLayer `json:"layers"`
+}
+
+func LoadScene(tilemapJson string) (*Tilemap, error) {
+	data, err := os.ReadFile("assets/tilesets/" + tilemapJson)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var tilemap Tilemap
+	json.Unmarshal(data, &tilemap)
+
+	return &tilemap, nil
 }
